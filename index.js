c343678c343cc47b176e6132531e4a2e4585e34d
Mohammad <mo7amed.2197@gmail.com>
2023-10-12 19:49:40 +0300

    Done

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const example = [
   ["A", "A", "A"],
-  ["A", "A", "A"],
-  ["A", "B", "A"]];
+  ["A", "A", "B"],
+  ["A", "A", "A"]];
 
 const whereIsWaldo = (matrix) => {
-  
+  const removeDuplicate = matrix.map(arr => [...new Set(arr)]);
+  console.log("01", removeDuplicate);
+  const locateRow = removeDuplicate.findIndex(el => el.length > 1)
+  console.log("02", locateRow);
+  const locateColumn = matrix[locateRow].findIndex(col => col != matrix[locateRow][0]);
+  console.log(locateColumn);
+  return [locateRow + 1, locateColumn + 1]
 }
+console.log(whereIsWaldo(example));
